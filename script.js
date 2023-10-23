2b048a8c02d79ab7ca07f47f43d2debb8841808e
elmar <elmarcera@gmail.com>
2023-10-23 16:29:13 +0800

    CHALLENGE #1: Video Solution

--- script.js
+++ script.js
@@ -165,24 +165,85 @@
 // Reviewing Functions
 ///////////////////////////////////////
 
-const calAge = function (birthYear) {
-  return 2037 - birthYear;
-};
+// const calAge = function (birthYear) {
+//   return 2037 - birthYear;
+// };
 
-const yearsUntilRetirement = function (birthYear, firstName) {
-  const age = calAge(birthYear);
-  const retirement = 65 - age;
+// const yearsUntilRetirement = function (birthYear, firstName) {
+//   const age = calAge(birthYear);
+//   const retirement = 65 - age;
 
-  if (retirement > 0) {
-    console.log(`${firstName} retires in ${retirement} years`);
-    return retirement;
-    console.log(`${firstName} retires in ${retirement} years`); ////cannot be reacch
+//   if (retirement > 0) {
+//     console.log(`${firstName} retires in ${retirement} years`);
+//     return retirement;
+//     console.log(`${firstName} retires in ${retirement} years`); ////cannot be reacch
+//   } else {
+//     console.log(`${firstName} has already retired`);
+//     return -1; // or return 99999   a number thats no meaning
+//     console.log(`${firstName} has already retired`); //cannot be reacch
+//   }
+// };
+
+// console.log(yearsUntilRetirement(1991, "Elmar"));
+// console.log(yearsUntilRetirement(1950, "Mike"));
+
+///////////////////////////////////////
+// JavaScript Fundamentals – Part 2
+// Coding Exercise 5: CHALLENGE #1
+///////////////////////////////////////
+
+// Back to the two gymnastics teams, the Dolphins and the Koalas! There is a new
+// gymnastics discipline, which works differently.
+// Each team competes 3 times, and then the average of the 3 scores is calculated (so
+// one average score per team).
+// A team only wins if it has at least double the average score of the other team.
+// Otherwise, no team wins!
+
+// Your tasks:
+// 1. Create an arrow function 'calcAverage' to calculate the average of 3 scores
+// 2. Use the function to calculate the average for both teams
+// 3. Create a function 'checkWinner' that takes the average score of each team
+// as parameters ('avgDolhins' and 'avgKoalas'), and then logs the winner
+// to the console, together with the victory points, according to the rule above.
+// Example: "Koalas win (30 vs. 13)"
+// 4. Use the 'checkWinner' function to determine the winner for both Data 1 and
+// Data 2
+// 5. Ignore draws this time
+
+// Test data:
+// § Data 1: Dolphins score 44, 23 and 71. Koalas score 65, 54 and 49
+// § Data 2: Dolphins score 85, 54 and 41. Koalas score 23, 34 and 27
+
+// Hints:
+// § To calculate average of 3 values, add them all together and divide by 3
+// § To check if number A is at least double number B, check for A >= 2 * B.
+// Apply this to the team's average scores �
+
+// GOOD LUCK �
+
+const calAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
+
+// Test1
+let scoreDolphins = calAverage(44, 23, 71);
+let scoreKoalas = calAverage(65, 54, 49);
+console.log(scoreDolphins, scoreKoalas);
+
+const checkWinner = function (avgDolphins, avgKoalas) {
+  if (avgDolphins >= avgKoalas * 2) {
+    console.log(`${"Dolphins"} win (${avgDolphins} vs ${avgKoalas}`);
+  } else if (avgKoalas >= avgDolphins * 2) {
+    console.log(`${"Koalas"} win (${avgDolphins} vs ${avgKoalas}`);
   } else {
-    console.log(`${firstName} has already retired`);
-    return -1; // or return 99999   a number thats no meaning
-    console.log(`${firstName} has already retired`); //cannot be reacch
+    console.log("No team wins!");
   }
 };
 
-console.log(yearsUntilRetirement(1991, "Elmar"));
-console.log(yearsUntilRetirement(1950, "Mike"));
+checkWinner(scoreDolphins, scoreKoalas);
+
+checkWinner(500, 150);
+
+// // Test2
+scoreDolphins = calAverage(85, 54, 41);
+scoreKoalas = calAverage(23, 34, 27);
+console.log(scoreDolphins, scoreKoalas);
+checkWinner(scoreDolphins, scoreKoalas);
