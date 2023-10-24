6cf97176087c6831f21a462209ba7063899588bd
elmar <elmarcera@gmail.com>
2023-10-24 14:55:06 +0800

    Dot vs. Bracket Notation

--- script.js
+++ script.js
@@ -413,21 +413,56 @@
 // Introduction to Objects
 ///////////////////////////////////////
 
-//Array
-const elmarArray = [
-  "Elmar",
-  "Angao",
-  2037 - 1985,
-  "teacher",
-  ["Michael", "Peter", "Steven"],
-];
+// //Array
+// const elmarArray = [
+//   "Elmar",
+//   "Angao",
+//   2037 - 1985,
+//   "teacher",
+//   ["Michael", "Peter", "Steven"],
+// ];
 
-// So there are multiple ways of creating objects.
-// But again, using the curly braces like this
-// is the easiest one,
-// because we are literally writing down
-// the entire object content.
+// // So there are multiple ways of creating objects.
+// // But again, using the curly braces like this
+// // is the easiest one,
+// // because we are literally writing down
+// // the entire object content.
 
+// //Object
+// const elmarObject = {
+//   firstName: "Elmar",
+//   lastName: "Angao",
+//   age: 2037 - 1985,
+//   job: "teacher",
+//   friends: ["Michael", "Peter", "Steven"],
+// };
+// // Key also called properties
+
+// // recap
+// // So to recap, just like arrays,
+// // we use objects to essentially group together
+// // different variables that really belong together
+// // such as the properties of Jonas
+// // that we've been working with.
+// // Now, the big difference between objects and arrays,
+// // is that in objects, the order of these values
+// // does not matter at all when we want to retrieve them.
+// // And that's important to keep in mind.
+
+// // So in arrays,
+// // the order in which we specify the elements matters a lot
+// // because that's how we access these elements, right?
+// // So we can only access array elements
+// // using their order number.
+// // This means that we should use arrays for more order data,
+// // and objects for more unstructured data.
+// // And data that we actually want to name,
+// // and then retrieve from the object,
+// // based on that name.
+
+///////////////////////////////////////
+// Dot vs. Bracket Notation
+///////////////////////////////////////
 //Object
 const elmarObject = {
   firstName: "Elmar",
@@ -436,26 +471,72 @@ const elmarObject = {
   job: "teacher",
   friends: ["Michael", "Peter", "Steven"],
 };
-// Key also called properties
 
-// recap
-// So to recap, just like arrays,
-// we use objects to essentially group together
-// different variables that really belong together
-// such as the properties of Jonas
-// that we've been working with.
-// Now, the big difference between objects and arrays,
-// is that in objects, the order of these values
-// does not matter at all when we want to retrieve them.
-// And that's important to keep in mind.
+console.log(elmarObject);
 
-// So in arrays,
-// the order in which we specify the elements matters a lot
-// because that's how we access these elements, right?
-// So we can only access array elements
-// using their order number.
-// This means that we should use arrays for more order data,
-// and objects for more unstructured data.
-// And data that we actually want to name,
-// and then retrieve from the object,
-// based on that name.
+console.log(elmarObject.lastName); //dot notation
+
+console.log(elmarObject["lastName"]); // Bracket notation
+
+// Bracket we can put any expresion
+const nameKey = "Name";
+console.log(elmarObject["first" + nameKey]);
+console.log(elmarObject["last" + nameKey]);
+
+// console.log(elmarObject."last" + nameKey);  //won't work
+
+// So in what situations should we use the dot notation
+// and when do we have to use deep brackets notation?
+// And I kind of already replied to that,
+// but this is just to recap.
+// So when we need to first compute the property name,
+// like we did here with the first and last name,
+// then of course we have to use the bracket notation
+// in any other case, just use the dot notation,
+// which looks a lot cleaner and it's also easier to use, okay.
+// And now to make the need for the bracket notation
+// even more clear, let me show you another example,
+// which I think is gonna be really fun.
+// So let's say that we don't know yet
+// which property we want to show,
+// and instead we get this information
+// from some user interface
+// so, for that we can use the prompt function.
+// Remember, so we can us
+
+const interestedIn = prompt(
+  "What do you want to know about Jonas? Choose between firstName, lastName, age, job, and friends"
+);
+console.log(elmarObject.interestedIn); //undefined
+console.log(elmarObject[interestedIn]);
+
+// And so let me tell you now that undefined is what we get
+// when we try to access a property
+// on an object that does not exist.
+
+if (elmarObject[interestedIn]) {
+  console.log(elmarObject[interestedIn]);
+} else {
+  console.log(
+    "Wrong request! Choose between firstName, LastName, age, job, and friends"
+  );
+}
+
+// add properties in object
+elmarObject.location = "Philippines";
+elmarObject["twitter"] = "@elmar";
+console.log(elmarObject);
+
+// CHALLENGES
+// "Elmar has 3 friends, and his best friend is called Michael"
+console.log(
+  elmarObject.firstName +
+    " has " +
+    elmarObject.friends.length +
+    " friends, and his best friend is called " +
+    elmarObject.friends[0]
+);
+
+console.log(
+  `${elmarObject.firstName} has ${elmarObject.friends.length} friends, and his best friend is called ${elmarObject.friends[0]}`
+);
