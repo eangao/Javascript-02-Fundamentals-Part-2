bbc78d7617ad159fd60929c483fa2065aa867ceb
elmar <elmarcera@gmail.com>
2023-10-24 12:05:15 +0800

    Introduction to Objects

--- script.js
+++ script.js
@@ -368,43 +368,94 @@
 // JavaScript Fundamentals – Part 2
 // Coding Exercise 6: CHALLENGE #2
 ///////////////////////////////////////
-// Steven is still building his tip calculator, using the same rules as before: Tip 15% of
-// the bill if the bill value is between 50 and 300, and if the value is different, the tip is
-// 20%.
+// // Steven is still building his tip calculator, using the same rules as before: Tip 15% of
+// // the bill if the bill value is between 50 and 300, and if the value is different, the tip is
+// // 20%.
 
-// Your tasks:
-// 1. Write a function 'calcTip' that takes any bill value as an input and returns
-// the corresponding tip, calculated based on the rules above (you can check out
-// the code from first tip calculator challenge if you need to). Use the function
-// type you like the most. Test the function using a bill value of 100
-// 2. And now let's use arrays! So create an array 'bills' containing the test data
-// below
-// 3. Create an array 'tips' containing the tip value for each bill, calculated from
-// the function you created before
-// 4. Bonus: Create an array 'total' containing the total values, so the bill + tip
+// // Your tasks:
+// // 1. Write a function 'calcTip' that takes any bill value as an input and returns
+// // the corresponding tip, calculated based on the rules above (you can check out
+// // the code from first tip calculator challenge if you need to). Use the function
+// // type you like the most. Test the function using a bill value of 100
+// // 2. And now let's use arrays! So create an array 'bills' containing the test data
+// // below
+// // 3. Create an array 'tips' containing the tip value for each bill, calculated from
+// // the function you created before
+// // 4. Bonus: Create an array 'total' containing the total values, so the bill + tip
 
-// Test data: 125, 555 and 44
+// // Test data: 125, 555 and 44
 
-// Hint: Remember that an array needs a value in each position, and that value can
-// actually be the returned value of a function! So you can just call a function as array
-// values (so don't store the tip values in separate variables first, but right in the new
-// array) �
-// GOOD LUCK �
+// // Hint: Remember that an array needs a value in each position, and that value can
+// // actually be the returned value of a function! So you can just call a function as array
+// // values (so don't store the tip values in separate variables first, but right in the new
+// // array) �
+// // GOOD LUCK �
 
-// Using arrow function
-// const calcTip = (bill) =>
-//   bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+// // Using arrow function
+// // const calcTip = (bill) =>
+// //   bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
 
-const calcTip = function (bill) {
-  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+// const calcTip = function (bill) {
+//   return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+// };
+// console.log(calcTip(100));
+
+// const bills = [125, 555, 44];
+// console.log(bills);
+
+// const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+// console.log(tips);
+
+// const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+// console.log(totals);
+
+///////////////////////////////////////
+// Introduction to Objects
+///////////////////////////////////////
+
+//Array
+const elmarArray = [
+  "Elmar",
+  "Angao",
+  2037 - 1985,
+  "teacher",
+  ["Michael", "Peter", "Steven"],
+];
+
+// So there are multiple ways of creating objects.
+// But again, using the curly braces like this
+// is the easiest one,
+// because we are literally writing down
+// the entire object content.
+
+//Object
+const elmarObject = {
+  firstName: "Elmar",
+  lastName: "Angao",
+  age: 2037 - 1985,
+  job: "teacher",
+  friends: ["Michael", "Peter", "Steven"],
 };
-console.log(calcTip(100));
+// Key also called properties
 
-const bills = [125, 555, 44];
-console.log(bills);
+// recap
+// So to recap, just like arrays,
+// we use objects to essentially group together
+// different variables that really belong together
+// such as the properties of Jonas
+// that we've been working with.
+// Now, the big difference between objects and arrays,
+// is that in objects, the order of these values
+// does not matter at all when we want to retrieve them.
+// And that's important to keep in mind.
 
-const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
-console.log(tips);
-
-const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
-console.log(totals);
+// So in arrays,
+// the order in which we specify the elements matters a lot
+// because that's how we access these elements, right?
+// So we can only access array elements
+// using their order number.
+// This means that we should use arrays for more order data,
+// and objects for more unstructured data.
+// And data that we actually want to name,
+// and then retrieve from the object,
+// based on that name.
