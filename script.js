c3c341260572d67dbadde608aa3343ad6a4ec46a
elmar <elmarcera@gmail.com>
2023-10-26 14:06:30 +0800

    Iteration: The for Loop

--- script.js
+++ script.js
@@ -632,66 +632,86 @@
 // Coding Exercise 7: CHALLENGE #3
 ///////////////////////////////////////////////////////
 
-// Let's go back to Mark and John comparing their BMIs! This time, let's use objects to
-// implement the calculations! Remember: BMI = mass / height ** 2 = mass
-// / (height * height) (mass in kg and height in meter)
+// // Let's go back to Mark and John comparing their BMIs! This time, let's use objects to
+// // implement the calculations! Remember: BMI = mass / height ** 2 = mass
+// // / (height * height) (mass in kg and height in meter)
 
-// Your tasks:
-// 1. For each of them, create an object with properties for their full name, mass, and
-// height (Mark Miller and John Smith)
-// 2. Create a 'calcBMI' method on each object to calculate the BMI (the same
-// method on both objects). Store the BMI value to a property, and also return it
-// from the method
-// 3. Log to the console who has the higher BMI, together with the full name and the
-// respective BMI. Example: "John's BMI (28.3) is higher than Mark's (23.9)!"
+// // Your tasks:
+// // 1. For each of them, create an object with properties for their full name, mass, and
+// // height (Mark Miller and John Smith)
+// // 2. Create a 'calcBMI' method on each object to calculate the BMI (the same
+// // method on both objects). Store the BMI value to a property, and also return it
+// // from the method
+// // 3. Log to the console who has the higher BMI, together with the full name and the
+// // respective BMI. Example: "John's BMI (28.3) is higher than Mark's (23.9)!"
 
-// Test data: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m
-// tall.
+// // Test data: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m
+// // tall.
 
-// GOOD LUCK �
+// // GOOD LUCK �
 
-const mark = {
-  fullName: "Mark Miller",
-  mass: 78,
-  height: 1.69,
+// const mark = {
+//   fullName: "Mark Miller",
+//   mass: 78,
+//   height: 1.69,
 
-  calcBMI: function () {
-    this.bmi = this.mass / (this.height * this.height);
-    return this.bmi;
-  },
-};
+//   calcBMI: function () {
+//     this.bmi = this.mass / (this.height * this.height);
+//     return this.bmi;
+//   },
+// };
 
-const john = {
-  fullName: "John Smith",
-  mass: 92,
-  height: 1.95,
+// const john = {
+//   fullName: "John Smith",
+//   mass: 92,
+//   height: 1.95,
 
-  calcBMI: function () {
-    this.bmi = this.mass / (this.height * this.height);
-    return this.bmi;
-  },
-};
+//   calcBMI: function () {
+//     this.bmi = this.mass / (this.height * this.height);
+//     return this.bmi;
+//   },
+// };
 
-mark.calcBMI();
-john.calcBMI();
-console.log(mark.bmi, john.bmi);
+// mark.calcBMI();
+// john.calcBMI();
+// console.log(mark.bmi, john.bmi);
 
-// console.log(
-//   `${john.fullName}'s BMI (${john.calcBMI()}) is ${
-//     john.calcBMI() > mark.calcBMI() ? "higher than" : "lower than"
-//   } ${mark.fullName}'s (${mark.calcBMI()})!`
-// );
+// // console.log(
+// //   `${john.fullName}'s BMI (${john.calcBMI()}) is ${
+// //     john.calcBMI() > mark.calcBMI() ? "higher than" : "lower than"
+// //   } ${mark.fullName}'s (${mark.calcBMI()})!`
+// // );
 
-if (mark.bmi > john.bmi) {
-  console.log(
-    `${mark.fullName}'s BMI (${mark.calcBMI()}) is higher than ${
-      john.fullName
-    }'s (${john.calcBMI()})!`
-  );
-} else if (john.bmi > mark.bmi) {
-  console.log(
-    `${john.fullName}'s BMI (${john.calcBMI()}) is higher than ${
-      mark.fullName
-    }'s (${mark.calcBMI()})!`
-  );
+// if (mark.bmi > john.bmi) {
+//   console.log(
+//     `${mark.fullName}'s BMI (${mark.calcBMI()}) is higher than ${
+//       john.fullName
+//     }'s (${john.calcBMI()})!`
+//   );
+// } else if (john.bmi > mark.bmi) {
+//   console.log(
+//     `${john.fullName}'s BMI (${john.calcBMI()}) is higher than ${
+//       mark.fullName
+//     }'s (${mark.calcBMI()})!`
+//   );
+// }
+
+////////////////////////////////////////////
+// Iteration: The for Loop
+////////////////////////////////////////////
+
+// console.log("Lifting weights repitation 1");
+// console.log("Lifting weights repitation 2");
+// console.log("Lifting weights repitation 3");
+// console.log("Lifting weights repitation 4");
+// console.log("Lifting weights repitation 5");
+// console.log("Lifting weights repitation 6");
+// console.log("Lifting weights repitation 7");
+// console.log("Lifting weights repitation 8");
+// console.log("Lifting weights repitation 9");
+// console.log("Lifting weights repitation 10");
+
+// for loop keeps running while condition is TRUE
+for (let rep = 1; rep <= 10; rep++) {
+  console.log(`Lifting weights repitation ${rep} `);
 }
